e965232b456d1212dff3ebb2f1986b2f41c355f3
kataraptor <kataraptor@gmail.com>
2016-12-03 17:45:59 -0600

    asteroid rotation

---
+++ Hack Day Project/Assets/GoogleVR/Scripts/RandomRotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+    public float tumble;
+    public float speed;
+    private Rigidbody rb;
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Random.insideUnitSphere * tumble;
+        transform.forward = -Camera.main.transform.forward;
+        rb.velocity = transform.forward * speed;
+    }
+}
