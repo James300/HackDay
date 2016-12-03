8f29c8b881401625d6ff3d23b2cf65463279848d
Scott Woodcock <scottw@iqmetrix.com>
2016-12-03 16:36:07 -0600

    bullets

---
+++ Hack Day Project/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject Bullet;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Input.GetMouseButtonDown(0))
+	    {
+	        var bullet = Instantiate(Bullet,transform.position,transform.rotation);
+            
+	    }
+	}
+}
